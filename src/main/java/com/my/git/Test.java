bfee807a8cdc35cd7ae51f8497133143a1217497
luhao <lh070960@gmail.com>
2020-10-12 11:43:38 +0800

    修改测试类3—>4

--- src/main/java/com/my/git/Test.java
+++ src/main/java/com/my/git/Test.java
@@ -10,6 +10,6 @@ public class Test {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
-        list.add(3);
+        list.add(4);
     }
 }
