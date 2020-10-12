7eac4d8bd7a3fc760b47c208e52e707707ebb27b
luhao <lh070960@gmail.com>
2020-10-12 11:40:22 +0800

    添加测试类

---
+++ src/main/java/com/my/git/Test.java
@@ -0,0 +1,15 @@
+package com.my.git;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: 陆浩
+ * @Date: 2020/10/12 11:38
+ */
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+    }
+}
